625671ee29596c7670f6b09e6c2d9006f6409eb6
vladlJS <vla-loskuto@yandex.ru>
2020-10-30 15:11:20 +0300

    Завершает игру при нажатии отмены

--- app2.js
+++ app2.js
@@ -21,6 +21,9 @@ function start(){
             num = prompt('Введите число от 1 до 100');
             if (isNaN(num)){
                 alert('Вы ввели не число!');
+            } else if (num === null) {
+                alert('Игра окончена!');
+                return 1;
             }
         }while(!isNumber(num));
     //let num = +prompt('Введите число от 1 до 100');
