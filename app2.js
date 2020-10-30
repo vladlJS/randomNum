2e15c94af762b0cdcbb1fd8c7b6c8d489b361930
vladlJS <vla-loskuto@yandex.ru>
2020-10-30 21:05:42 +0300

    new feat

--- app2.js
+++ app2.js
@@ -24,7 +24,7 @@ function start(){
                 alert('Игра окончена!');
                 return 1;
             }
-        }while(!isNumber(num) || num > 101 || num < 1);
+        }while(!isNumber(num) || num > 100 || num < 1);
     function number(){
         if (num > randomNum){
             res = confirm('Ваше число больше');
